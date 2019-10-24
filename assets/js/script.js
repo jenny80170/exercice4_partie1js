780f8bab5c8b61b02dcb8235ed0fd86d2100ab8d
jenny80170 <jennifer.malleret@outlook.fr>
2019-10-24 09:32:33 +0200

    exercice 4 partie 1 js

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+function validate() {
+  var lastName = document.getElementById('lastname').value;
+  var firstName = document.getElementById('firstname').value;
+  var city = document.getElementById('city').value;
+  window.alert('Nom : ' + lastName + '\n Prénom : ' + firstName + '\n Ville : ' + city);
+}
